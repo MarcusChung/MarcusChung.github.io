b2f479de07944a36b5c3aa1d4ef02735381ffbbd
Marcus Chung <marcuschung2002@gmail.com>
2025-03-12 21:49:53 +0000

    add base to astro.configs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,11 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 const URL = 'https://MarcusChung.github.io';
 // https://astro.build/config
 export default defineConfig({
+    base: "/",
     site: URL,
     vite: {
         plugins: [tailwindcss()],
     },
-    trailingSlash: "always", // Ensures static file paths are correct
+     // Ensures static file paths are correct
     output: "static", // Ensures full static output for GitHub Pages
     // define: {
     //     'import.meta.env.FMP_API_KEY': process.env.FMP_API_KEY,
